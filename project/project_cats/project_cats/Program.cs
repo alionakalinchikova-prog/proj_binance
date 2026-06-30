a28662d0b3b485b96b2050772b0744ca0f15661f
Aliona Kalinchikova <alionakalinchikova@gmail.com>
2026-06-30 15:29:02 +0300

    курс биткоина к доллару

--- project/project_cats/project_cats/Program.cs
+++ project/project_cats/project_cats/Program.cs
@@ -7,29 +7,16 @@ class Program
 {
     static async Task Main()
     {
-        Console.WriteLine(" Курс криптовалюты к доллару"); 
-        Console.WriteLine(new string('-', 30));
-
         using (HttpClient client = new HttpClient())
         {
-            //string url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"; //биткоин
-            string url = "https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT";  // эфир
-            //string url = "https://api.binance.com/api/v3/ticker/price?symbol=BNBUSDT";   // BNB
-           //string url = "https://api.binance.com/api/v3/ticker/price?symbol=DOGEUSDT";  // догикойн*
-
+            string url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT";
             string json = await client.GetStringAsync(url);
 
             using JsonDocument doc = JsonDocument.Parse(json);
             JsonElement root = doc.RootElement;
 
-            string symbol = root.GetProperty("symbol").GetString();
             string price = root.GetProperty("price").GetString();
-
-            Console.WriteLine($"\n{symbol}: {price} $");
-
-            string fileName = $"price_{DateTime.Now:yyyy-MM-dd_HHmm}.json";
-            System.IO.File.WriteAllText(fileName, json);
-            Console.WriteLine($"\n Сохранено в {fileName}");
+            Console.WriteLine($"BTC: {price} $");
         }
     }
 }
